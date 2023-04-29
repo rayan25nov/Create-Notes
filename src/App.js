d00041f1c069edea8fc845722d55df27dbe6d18e
Rayan Ahmad <rayan25nov@gmail.com>
2023-04-29 17:32:17 +0530

    changed some css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core";
 import { purple } from "@material-ui/core/colors";
 import Layout from "./UI/Layout";
-
 
 const theme = createTheme({
   palette: {
@@ -28,14 +28,16 @@ function App(props) {
     <ThemeProvider theme={theme}>
       <Router>
         <Layout>
-          <Switch>
-            <Route exact path="/">
-              <Notes />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-          </Switch>
+          <Container>
+            <Switch>
+              <Route exact path="/">
+                <Notes />
+              </Route>
+              <Route path="/create">
+                <Create />
+              </Route>
+            </Switch>
+          </Container>
         </Layout>
       </Router>
     </ThemeProvider>
